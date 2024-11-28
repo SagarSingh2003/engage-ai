3101a5701c55b1adfdff2c6840eaf7ceb5d77007
SagarSingh2003 <iamsagar762@gmail.com>
2024-11-28 16:21:18 +0530

    add mock data

---
+++ src/data/mockdata.ts
@@ -0,0 +1,77 @@
+export const statsData = [
+    {
+      title: "Daily Active users",
+      periods: [
+        {
+          label: "Today",
+          date: "26th Nov",
+          value: "12.4k"
+        },
+        {
+          label: "Yesterday",
+          date: "25th Nov",
+          value: "7.8k"
+        }
+      ]
+    },
+    {
+      title: "Weekly Active users",
+      periods: [
+        {
+          label: "This Week",
+          date: "21st Nov to 26th Nov",
+          value: "12.4k"
+        },
+        {
+          label: "Last Week",
+          date: "15th Nov to 21st Nov",
+          value: "7.8k"
+        }
+      ]
+    },
+    {
+      title: "Montly Active users",
+      periods: [
+        {
+          label: "This Month",
+          date: "1st Nov to 26th Nov",
+          value: "12.4k"
+        },
+        {
+          label: "Yesterday",
+          date: "1st Oct to 31st Oct",
+          value: "7.8k"
+        }
+      ]
+    },
+    {
+      title: "Montly Active users",
+      periods: [
+        {
+          label: "This Month",
+          date: "1st Nov to 26th Nov",
+          value: "12.4k"
+        }  
+      ]
+    },
+    {
+      title: "Montly Active users",
+      periods: [
+        {
+          label: "This Month",
+          date: "1st Nov to 26th Nov",
+          value: "12.4k"
+        }  
+      ]
+    },
+    {
+      title: "Montly Active users",
+      periods: [
+        {
+          label: "This Month",
+          date: "1st Nov to 26th Nov",
+          value: "12.4k"
+        }  
+      ]
+    }
+  ];
